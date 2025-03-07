d3b1ca5dba347c37e9890a8714d793941609f0fa
amazingazry <aazri1121@gmail.com>
2025-03-08 01:25:17 +0800

    read and display data from excel to c#

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,39 @@
+using System;
+using System.Text;
+using ClosedXML.Excel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "gila World!");
+app.MapGet("/", () =>
+{
+    return DisplayData("./excel/patient.xlsx");
+});
 
 app.Run();
+
+static string DisplayData(string filepath)
+{
+    StringBuilder output = new StringBuilder();
+    
+    // Open the Excel File
+    using (var workbook = new XLWorkbook(filepath))
+    {
+        var worksheet = workbook.Worksheet(1);
+        var range = worksheet.RangeUsed();
+
+        foreach (var row in range.Rows())
+        {
+            foreach (var cell in row.Cells())
+            {
+                output.Append(cell.Value + "\t");
+            }
+            output.AppendLine();
+        }
+    }
+    
+    return output.ToString();
+}
